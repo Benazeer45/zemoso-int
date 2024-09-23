635432c92aaddfa7f12e45ee679775ed05421551
Shaik Benazeer <shaik.benazeer@zemosolabs.com>
2024-09-23 11:19:19 +0530

    Updated app files

--- sonar-cloud/src/App.test.tsx
+++ sonar-cloud/src/App.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // For extended matchers like toBeInTheDocument
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react text', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
