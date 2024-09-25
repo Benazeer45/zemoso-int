7b8e3dc339db6374c97a27fef117fe599df921fc
Shaik Benazeer <shaik.benazeer@zemosolabs.com>
2024-09-25 21:14:10 +0530

    updated jest file

--- parent-child/jest.config.js
+++ parent-child/jest.config.js
@@ -1,38 +1,20 @@
-// module.exports = {
-// 	verbose: true,
-// 	globals: {
-// 	  'ts-jest': {
-// 		diagnostics: true,
-// 	  },
-// 	},
-// 	transform: {
-// 	  '^.+\\.(js|ts|tsx)?$': 'ts-jest',
-// 	  '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-// 		'jest-transform-stub',
-// 	  '\\.(yaml)$': 'jest-raw-loader',
-// 	},
-// 	transformIgnorePatterns: ['node_modules/(?!@prekari/core-ui)'],
-// 	moduleNameMapper: {
-// 	  '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-// 	},
-// 	testEnvironment: 'jest-environment-jsdom',
-// 	//setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-//   };
-
 module.exports = {
-    verbose: true,
-    transform: {
-      '^.+\\.(js|ts|tsx)?$': ['ts-jest', {
-        diagnostics: true,
-      }],
-      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub',
-      '\\.(yaml)$': 'jest-raw-loader',
-    },
-    transformIgnorePatterns: ['node_modules/(?!@prekari/core-ui)'],
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-    testEnvironment: 'jest-environment-jsdom',
-    // setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+	verbose: true,
+	globals: {
+	  'ts-jest': {
+		diagnostics: true,
+	  },
+	},
+	transform: {
+	  '^.+\\.(js|ts|tsx)?$': 'ts-jest',
+	  '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+		'jest-transform-stub',
+	  '\\.(yaml)$': 'jest-raw-loader',
+	},
+	transformIgnorePatterns: ['node_modules/(?!@prekari/core-ui)'],
+	moduleNameMapper: {
+	  '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+	},
+	testEnvironment: 'jest-environment-jsdom',
+	//setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   };
-  
